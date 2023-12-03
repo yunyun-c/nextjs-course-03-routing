984b5d0d1f442f31139a2892a04e519f76320355
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-12-02 22:55:11 -0600

    add onSearch function

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,23 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
-import { Fragment } from "react";
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function FindEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={FindEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
